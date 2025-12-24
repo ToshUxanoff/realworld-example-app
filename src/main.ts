8056b5af5e8ab4788cabdabf2bf6b234a5cbf056
ToshUxanoff <toshuxanoff@gmail.com>
2025-12-24 12:58:28 +0100

    change port to 9000

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ app.use(
  * Server activation
  */
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.info(`server up on port ${PORT}`);
